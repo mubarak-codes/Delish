fcabdf85697b06e3d4456e29923839fe8f9b68cc
mubarak-codes <mubarakcodes@gmail.com>
2025-04-19 23:12:07 +0100

    gg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,9 @@ const Hero = ({bg, text})=>{
 
  return(
   <>
-   <section className={`${bg} ${text} px3 px4-sm mt17  hover-bg-red50`}>
+   <section className={`${bg} ${text} px3 px4-sm mt17  hover-bg-red50`} style={{backgroundImage:'url(img/HeroBackground.jpg)',backgroundSize:'auto'}}>
     <h1>Delish</h1>
+    <h2>ggg</h2>
    </section>
   </>
  )
