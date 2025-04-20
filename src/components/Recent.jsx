d9bce38371c1edc4b164d6a4a2d8ec55f764d318
mubarak-codes <mubarakcodes@gmail.com>
2025-04-20 22:27:55 +0100

    recent

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -8,10 +8,12 @@ const Recent = ()=>{
    <section className="w100 mt5 bg-red10 px4 px5-sm py4">
     <h3 className="ff-rubik text-center fs7 mb6 ">Recent Recipes</h3>
     <div className="d-grid gr2-sm">
-     <div className="text-break">
+     <div className="text-break relative">
+      <i className="bi bi-star absolute top-0 end-0 m3 fs4 bg-gray75 rounded-circle px1" onClick={}></i> 
       <img src="img/HeroBackground.jpg" className="w100 rounded-3 mb6" />
       <h5 className="fw-bolder fs3">Different food on table.</h5>
-      <div className="fs2 opacity-75">ggghggggggggghhhggggvvvgggghhhhhgggggvfffgggggggggggr56ygggccxderr443eesddfgghhhhhhuii8887yyygggggggffffftttyy6hhbbff</div>
+      <div className="fs2 opacity-75 mb2">ggghggggggggghhhggggvvvgggghhhhhgggggvfffgggggggggggr56ygggccxderr443eesddfgghhhhhhuii8887yyygggggggffffftttyy6hhbbff</div>
+      <div className="fs1 opacity-75">10 hours ago</div>
      </div>
     </div>
    </section>
