e34e7da5f289fd5bac7386f96f6ede3ba94b5894
mubarak-codes <mubarakcodes@gmail.com>
2025-04-23 13:04:18 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,24 @@ const Footer = ()=>{
 
  return(
   <>
-   <section className="mt13">
-    <div>
-      <div className="fs4 p2 d-flex justify-content-center gap-3 gap-lg-4 mt2 border-top border-2 pt5">
-       <i className="fa-brands fa-youtube hover-text-orange transit"></i> 
-       <i className="fa-brands fa-facebook hover-text-orange transit"></i>
-       <i className="fa-brands fa-instagram hover-text-orange transit"></i>
-       <i className="fa-brands fa-tiktok hover-text-orange transit"></i>
-       <i className="fa-solid fa-phone hover-text-orange transit"></i>
-       <i className="fa-solid fa-envelope hover-text-orange transit"></i>
-      </div>   
+   <section className="mt13 border-top border-2 py4 px4 px5-sm">
+    <div className="">
+     <div className="fs4 d-flex justify-content-center gap-3 gap-lg-4 my3 mb6">
+      <i className="fa-brands fa-youtube hover-text-orange transit"></i> 
+      <i className="fa-brands fa-facebook hover-text-orange transit"></i>
+      <i className="fa-brands fa-instagram hover-text-orange transit"></i>
+      <i className="fa-brands fa-tiktok hover-text-orange transit"></i>
+      <i className="fa-solid fa-phone hover-text-orange transit"></i>
+      <i className="fa-solid fa-envelope hover-text-orange transit"></i>
+     </div>
+     <div className="fs2 text-nowrap flex gap-3">
+      <a href="/favorite" className="">Favorite</a>
+      <a href="/add-recipe" className="">Add Recipe</a>
+      <a href="/about" className="">About</a>
+      <a href="/help" className="">Help & Support</a>
+     </div>  
+
+     <div className="fs1 text-center mt6 opacity75">© Delish 2025</div>
     </div>
    </section>
   </>
