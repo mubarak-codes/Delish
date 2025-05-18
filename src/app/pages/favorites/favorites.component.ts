fac2517e58251a448572713ca8117a8e572bbb23
mubarak-codes <mubarakcodes@gmail.com>
2025-05-18 22:06:55 +0100

    new

---
+++ src/app/pages/favorites/favorites.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [],
+  template: `
+    <p>
+      favorites works!
+    </p>
+  `,
+  styles: ``
+})
+export class FavoritesComponent {
+
+}
